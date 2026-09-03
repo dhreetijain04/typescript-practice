7eed0b35dbb2a02883cade82474215f8f0a5f148
dhreetijain04 <dhreetijain04@gmail.com>
2026-09-03 18:49:32 +0530

    type

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+function greet(person: string):string{
+    return `Hello ${person}, welcome to TypeScript!`
+}
+const username :string = "dhreetii"
+console.log(greet(username))
---
+++ src/typesInTs.ts
@@ -0,0 +1,10 @@
+let drink ="chai"
+
+let cups = Math.random() > 0.5 ? 10: 5
+
+let channelName = "dhreeti"
+
+let chaiflavour: string = "masala"
+chaiflavour="ginger"
+
+let chaiOrder:number
