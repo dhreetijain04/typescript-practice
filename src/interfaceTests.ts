9937b4f57a23b8c23cb4ed02de26f9a19d8ec8f3
dhreetijain04 <dhreetijain04@gmail.com>
2026-09-06 17:43:54 +0530

    typescript prac

---
+++ src/interfaceTests.ts
@@ -0,0 +1,10 @@
+
+type chaiOrder = {type: string, sugar: number, strong: boolean};
+
+function makeChai(order: chaiOrder){
+    console.log(order);
+}
+
+function serveChai(order:chaiOrder){
+    console.log(order);
+}
--- src/moreTypes.ts
+++ src/moreTypes.ts
@@ -11,3 +11,28 @@ let bookString = '{"name": "One thing" }';
 let bookObject = JSON.parse(bookString) as book
 
 console.log(bookObject);
+
+const inputElement = document.getElementById("username") as HTMLInputElement
+
+let value:any
+value = "chai"
+value = [1,2,3]
+value = 2.5
+value.toUpperCase()
+
+let newVal: unknown
+newVal = "chai"
+newVal = [1,2,3]
+newVal = 2.5
+if(typeof newVal === "string"){
+    newVal.toUpperCase()
+}
+
+try {
+    
+} catch (error) {
+    if(error instanceof Error){
+        console.log(error.message)
+    }
+    console.log("error",error);
+}
